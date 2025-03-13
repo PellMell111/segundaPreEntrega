4bb98bb8f2afd261b90e5374a91bf5c4a133da2b
PellMell777 <pedrourzuamartelli@gmail.com>
2025-03-13 15:32:59 -0300

    Re-entrega

--- app.js
+++ app.js
@@ -1,15 +1,48 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
 const app = express();
-const productsRouter = require('./routes/products');
-const cartsRouter = require('./routes/carts');
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
 const PORT = 8080;
 
+app.engine('handlebars', engine({
+    defaultLayout: 'main',
+}));
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/products', productsRouter);
-app.use('/api/carts', cartsRouter);
+app.get('/', (req, res) => {
+    const products = require('./data/products.json');
+    res.render('home', { products, title: 'Home' });
+});
 
-app.listen(PORT, () => {
+app.get('/realtimeproducts', (req, res) => {
+    const products = require('./data/products.json');
+    res.render('realTimeProducts', { products, title: 'Productos en Tiempo Real' });
+});
+
+io.on('connection', (socket) => {
+    console.log('Un cliente se ha conectado');
+
+    socket.on('addProduct', (product) => {
+        const productManager = require('./managers/productManager');
+        productManager.addProduct(product);
+        io.emit('updateProducts', productManager.getProducts());
+    });
+
+    socket.on('deleteProduct', (id) => {
+        const productManager = require('./managers/productManager');
+        productManager.deleteProduct(id);
+        io.emit('updateProducts', productManager.getProducts());
+    });
+});
+
+server.listen(PORT, () => {
     console.log(`Servidor en funcionamiento en http://localhost:${PORT}`);
 });
